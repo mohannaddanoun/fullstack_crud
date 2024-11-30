0602f021b3504962ac6fc007ad6946f0dc0cc083
mohannaddanoun <danoun.mohanad@gmail.com>
2024-11-30 10:25:04 +0300

    Initial commit for Full Stack Project

---
+++ Api/Contactly/Models/AddContactRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Contactly.Models
+{
+    public class AddContactRequestDto
+    {
+        public required string Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public required string Phone { get; set; }
+
+        public bool Favorite { get; set; }
+    }
+}
